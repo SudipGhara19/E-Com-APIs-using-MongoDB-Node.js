4292302024dae7ff191c5c2c570b8f1e957f0e44
SudipGhara19 <sudipghara135@gmail.com>
2024-05-02 18:43:01 +0530

    updated the upgrading quantity by updateOne functions -upsert- option

--- src/features/cartitems/cartitems.repository.js
+++ src/features/cartitems/cartitems.repository.js
@@ -12,7 +12,12 @@ export default class CartItemsRepository{
         try{
         const db = getDB();
         const collection = db.collection(this.collection);
-        await collection.insertOne({productID: new ObjectId(productID), userID: new ObjectId(userID), quantity})
+        await collection.updateOne({productID: new ObjectId(productID), userID: new ObjectId(userID)},
+        {$inc: {
+            quantity: quantity
+        }},
+        {upsert: true})
+
         }catch(err){
             console.log(err);
             throw new ApplicationError("Something wrong with the database.", 500);
